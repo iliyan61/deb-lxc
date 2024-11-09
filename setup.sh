77c833509112870ae9e6bfb2ba96a0f76518ef04
Iliyan Jivraj <me@iliyan.dev>
2024-11-09 10:48:59 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt upgrade -y
 apt autoremove -y
 
 # Install required packages
-apt install -y sudo curl ca-certificates
+apt install -y sudo curl ca-certificates gpg
 
 # Remove any conflicting Docker packages
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do 
@@ -23,8 +23,16 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+#add fish repo
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+
 # Update package list to include Docker's packages
-sudo apt-get update
+sudo apt update
 
 # Install Docker Engine and related packages
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin fish
+
+
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
